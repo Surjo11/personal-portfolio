007c0403e2b92c0f1d3a253ff9803a38d49f5810
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-06-12 23:26:53 +0600

    finishing touch on contact

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,6 @@ export default function Home() {
   const workRef = useRef();
   const aboutRef = useRef();
   const contactRef = useRef();
-  // const nameRef = useRef("");
-  // const emailRef = useRef("");
-  // const messageRef = useRef("");
 
   // Handling Scroll
   const handleWorkScroll = () => {
@@ -43,12 +40,6 @@ export default function Home() {
   const handelSubmit = (e) => {
     e.preventDefault();
     console.log(e);
-    // const name = nameRef.current.value;
-    // // console.log(name);
-    // const email = emailRef.current.value;
-    // // console.log(email);
-    // const message = messageRef.current.value;
-    // // console.log(message);
     emailjs.sendForm(
       "service_v7y5z3a",
       "template_8e5csda",
@@ -122,47 +113,19 @@ export default function Home() {
           ))}
         </div>
       </div>
-      {/* Contact me Start */}
+      {/* Contact Start */}
       <section
         className="mt-28 mob:mt-10 laptop:mt-28 mob:p-2 laptop:p-0 py-6"
         ref={contactRef}
       >
-        <div className="mt-10 mob:mt-5 laptop:mt-10 grid max-w-6xl grid-cols-1 px-6 mx-auto laptop:px-8 laptop:grid-cols-2 laptop:divide-x">
-          <div className="py-6 laptop:py-0 laptop:px-6">
-            <h1 className="text-4xl font-bold">Get in touch</h1>
-            <p className="pt-2 pb-4">
-              Fill in the form to start a conversation
-            </p>
-            <div className="space-y-4">
-              <p className="flex items-center">
-                <span>Shantahar,Bogura</span>
-              </p>
-              <p className="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 mr-2 mob:mr-6"
-                >
-                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
-                </svg>
-                <span>+8801771-842603</span>
-              </p>
-              <p className="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 mr-2 mob:mr-6"
-                >
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                </svg>
-                <span>abdullah.safyet11@gmail.com</span>
-              </p>
-            </div>
-          </div>
-          {/* Contact Form Start */}
+        <h1 className="mob:text-2xl laptop:text-5xl text-bold pl-2">
+          Contact.
+        </h1>
+        <div className="mt-10 mob:mt-5 laptop:mt-10 grid max-w-6xl grid-cols-1 px-6 mx-auto laptop:px-8 laptop:grid-cols-2 laptop:items-center">
+          <img
+            src="https://i.postimg.cc/KvJcK3BJ/Mail-sent-rafiki.png"
+            alt=""
+          />
           <form
             onSubmit={handelSubmit}
             className="flex flex-col py-6 space-y-6 laptop:py-0 laptop:px-6"
@@ -172,7 +135,6 @@ export default function Home() {
               <input
                 type="text"
                 name="name"
-                // ref={nameRef}
                 placeholder="Abdullah Al Safayet"
                 className="block w-full rounded-md shadow-sm focus:ring-opacity-75 focus:ring-gray-800"
               />
@@ -182,7 +144,6 @@ export default function Home() {
               <input
                 type="email"
                 name="userEmail"
-                // ref={emailRef}
                 placeholder="abdullah.safayet@gmail.com"
                 className="block w-full rounded-md shadow-sm focus:ring-opacity-75 focus:ring-gray-800"
               />
@@ -191,7 +152,6 @@ export default function Home() {
               <span className="mb-1">Message</span>
               <textarea
                 name="message"
-                // ref={messageRef}
                 rows="3"
                 className="block w-full rounded-md shadow-sm focus:ring-opacity-75 focus:ring-gray-800"
               ></textarea>
